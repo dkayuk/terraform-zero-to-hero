a64425105662174e979d1c4db8eee179f9b8c707
dkayuk <41592675+dkayuk@users.noreply.github.com>
2024-07-22 13:04:51 +0000

    change 1.0

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "us-east-1"  # Set your desired AWS region v2
 }
 
 resource "aws_instance" "example" {
